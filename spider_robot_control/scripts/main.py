4b9e7dd109d8e78115d07ff3f3b62c9ede2fb8c3
Ramil <ramilaidarovich@gmail.com>
2019-11-12 14:13:58 +0300

    also modified control package from previous assignment

--- spider_robot_control/scripts/main.py
+++ spider_robot_control/scripts/main.py
@@ -16,6 +16,8 @@ def talker():
 	pub333 = rospy.Publisher('/spider_robot/leg33_leg333_position_controller/command',Float64,queue_size=10)
 	pub444 = rospy.Publisher('/spider_robot/leg44_leg444_position_controller/command',Float64,queue_size=10)
 
+	pub_cam = rospy.Publisher('/spider_robot/camera_joint_position_controller/command',Float64,queue_size=10)
+
 	rospy.init_node('talker', anonymous=True)
 	rate = rospy.Rate(50)
 
@@ -34,6 +36,8 @@ def talker():
 		position333 = random.uniform(-0.5,0.5)
 		position444 = random.uniform(-0.5,0.5)
 
+		positioncam = random.uniform(-1,1)
+
 
 		pub11.publish(position11)
 		pub22.publish(position22)
@@ -44,8 +48,8 @@ def talker():
 		pub222.publish(position222)
 		pub333.publish(position333)
 		pub444.publish(position444)
-
-
+	
+		pub_cam.publish(positioncam)
 
 		rate.sleep()
 
